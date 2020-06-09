7ee3f152ea995f3029bdde32e0e644119d2379e6
Jared Lidenberg <jaredl101@hotmail.com>
2020-06-09 12:08:23 -0500

    Contact working

---
+++ src/components/Contact/Contact.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Contact extends Component {
+    render() {
+        return (
+            <div>
+                <h2>Contact</h2>
+                <p>123-456-7890</p>
+                <p>johndoe@whatever.com</p>
+            </div>
+        );
+    }
+}
+
+export default Contact;
